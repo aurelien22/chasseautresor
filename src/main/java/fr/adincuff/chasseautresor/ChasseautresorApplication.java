ac335bb19cb44f75b8e1dee0cab826754915c138
aurelien22 <aurelien.dincuff@gmail.com>
2021-06-22 17:46:21 +0200

    first commit

---
+++ src/main/java/fr/adincuff/chasseautresor/ChasseautresorApplication.java
@@ -0,0 +1,13 @@
+package fr.adincuff.chasseautresor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChasseautresorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChasseautresorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/adincuff/chasseautresor/ChasseautresorApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.adincuff.chasseautresor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChasseautresorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
